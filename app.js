67ead08ef83a3a0bdc7e0c42dc4468e668dc42cb
tonraki <Keattipong.ng57@snru.ac.th>
2023-07-15 14:50:44 +0700

    update 15/07/2023

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ class User{
     //property ข่้อมูล class เริ่มต้นจะเป็น public เสมอหากต้องการให้เป็น pivate ให้ใส่ # หน้าชื่อ
     #name
     #password
+    type="ผู้ใช้งานระบบ" //เพิ่ม property type
 
     //default constructor 
     // constructor(){
@@ -13,17 +14,26 @@ class User{
         this.#name=name
         this.#password=password
         // this.showDetail() 
-    }
+    }   
     //method
     showDetail(){
         console.log(`ชื่อผู้ใช้ ${this.#name}, รหัสผ่าน ${this.#password}`)
     }
-    setName(newName){ //method setName() ในกรณีที่ต้องการแก้ไขข้อมูล private
+    //setter method คือในกรณีที่ต้องการแก้ไขข้อมูล private
+    set Name(newName){ 
         this.#name = newName
     }
-    getName(){
-        return this.#name //กรณีที่ต้องการเรียกข้อมูลเฉพาะออกมาแสดงเรียกใช้ method getName()
+    //getter method คือในกรณีที่ต้องการเรียกข้อมูลเฉพาะออกมาแสดง
+    get Name(){
+        return this.#name 
     }
+    set Password(newPassword){
+        this.#password = newPassword
+    }
+    get Password(){
+        return this.#password
+    }
+    //Accessors ต่างจาก method get และ set ปกติตรงที่เราเรียกการใช้งานผ่าน property ได้เลยโดยไม่ต้องเรียกใช้ผ่านฟังชันก์ setName 
 }
 
 // // สร้าง Object
@@ -38,7 +48,17 @@ class User{
 // console.log(user2.password)
 
 const user1=new User("non",1234)
-user1.setName("Keattipong")
-// user1.showDetail() //ใช้กรณีระบุนอก class
-console.log(user1.getName()) //กรณีที่ต้องการเรียกข้อมูลเฉพาะออกมาแสดงเรียกใช้ method getName()
-
+const user2=new User("ton",4321)
+const user3=new User("noina",1111)
+console.log(user1.type) //เรียกใช้งานการแสดงผลโดยทั่วไปโดยดึงค่า object type มาแสดงผ่าน console.log
+console.log(user2.type) //เรียกใช้งานการแสดงผลโดยทั่วไปโดยดึงค่า object type มาแสดงผ่าน console.log
+console.log(user3.type) //เรียกใช้งานการแสดงผลโดยทั่วไปโดยดึงค่า object type มาแสดงผ่าน console.log
+// user1.setName("Keattipong")
+// user1.setPassword("0989")
+// // user1.showDetail() //ใช้กรณีระบุนอก class
+// console.log(user1.getName()) //กรณีที่ต้องการเรียกข้อมูลเฉพาะออกมาแสดงเรียกใช้ method ผ่าน get function
+// console.log(user1.getPassword()) //กรณีที่ต้องการเรียกข้อมูลเฉพาะออกมาแสดงเรียกใช้ method ผ่าน get function
+// user1.Name="Ton naja"
+// user1.Password=4444
+// console.log(user1.Name) //เรียกใช้งาน get Name() ในกรณีที่ไม่มีการกำหนดค่า >> setName()
+// console.log(user1.Password) //เรียกใช้งาน get Name() ในกรณีที่ไม่มีการกำหนดค่า >> setName()
